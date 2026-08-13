a9b1052651d2929cb9b32a997a2798729f91ddee
Prof. Felipe Vilella <proffelipe.oliveira@fiap.com.br>
2026-08-13 08:58:46 -0300

    atualiza script aci

--- scripts/aci.sh
+++ scripts/aci.sh
@@ -1,11 +1,27 @@
+REGISTRY=2tdsapf2128
+RG_NAME=rg-pf2128-aci
+LOCATION=eastus
+
+az group create --name $RG_NAME --location $LOCATION
+
+ACR_USERNAME=$(az acr credential show \
+  --name $REGISTRY \
+  --query username \
+  --output tsv)
+
+ACR_PASSWORD=$(az acr credential show \
+  --name $REGISTRY \
+  --query 'passwords[0].value' \
+  --output tsv)
+
 az container create \
-    --resource-group rg-pf2128-acr \
+    --resource-group $RG_NAME \
     --name guacamole-api \
-    --image 2tdsapf2128.azurecr.io/guacamole:v4 \
+    --image $REGISTRY.azurecr.io/guacamole:v1 \
     --cpu 1 \
     --memory 2 \
     --ports 8080 \
-    --registry-username 2tdsapf2128 \
-    --registry-password $REGISTRY_PASSWORD \
+    --registry-username $ACR_USERNAME \
+    --registry-password $ACR_PASSWORD \
     --os-type Linux \
     --ip-address Public
